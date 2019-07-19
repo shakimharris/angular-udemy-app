cfaa1c161025a8625973d0ec454a2d1afddab8b5
amenraprod <amenraprod@gmail.com>
2019-07-19 16:35:13 -0400

    added log function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+function log (target, name, descriptor){
+  console.log(target, name, descriptor);
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloworld';
+  title = 'Udemy App';
 }
