25089001ab9040b0e1d112db9c5761c626b60df3
amenraprod <amenraprod@gmail.com>
2019-07-21 18:15:10 -0400

    two new comp. for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { HomeComponent } from './home/home.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent
+    HelloComponent,
+    HomeComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
