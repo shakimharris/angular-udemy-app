e1d8852b0b7f27487a271e879d3a33259fed15e5
amenraprod <amenraprod@gmail.com>
2019-07-21 13:47:47 -0400

    removed old data

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -9,35 +9,6 @@ export class RecordsService {
   constructor(private http: HttpClient]) { }
 
   getData() {
-    return [
-      {
-        name: 'Shakim',
-        address: '176 John Daly'
-      },
-        {
-        name: 'Kristina', 
-        address: '165 Vincent'
-      },
-      {
-        name: 'Shakim',
-        address: '176 John Daly'
-      },
-        {
-        name: 'Kristina', 
-        address: '165 Vincent'
-      },
-      {
-        name: 'Shakim',
-        address: '176 John Daly'
-      },
-        {
-        name: 'Kristina', 
-        address: '165 Vincent'
-        },
-        {
-        name: 'Rihanna',
-        address: false
-      }
-    ]
+    return 
   }
 }
