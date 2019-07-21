611d3ec8badb17e67c2a8f8f32db00202a2202aa
amenraprod <amenraprod@gmail.com>
2019-07-21 15:37:19 -0400

    corrected data source

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -9,7 +9,7 @@ export class RecordsService {
   constructor(private http: HttpClient) { }
 
   getData() {
-    return this.http.get('http://localhost:3400/file.json')
+    return this.http.get('http://localhost:3400/file.php')
     .subscribe(data => {
       console.log('We got', data);
     });
