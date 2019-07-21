d1b31d2cedf801d4f1e9701edfe4d88a111836b4
amenraprod <amenraprod@gmail.com>
2019-07-21 03:23:43 -0400

    removed updatevalue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  test = 'Udemy App';
+  text = 'Udemy App';
 
-  updateValue(e){
-  console.log(e);
-  }
 }
