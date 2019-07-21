fc82f04bb81cc6f5943252fe97c5b8e0b821045a
amenraprod <amenraprod@gmail.com>
2019-07-21 13:32:39 -0400

    added http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RecordsService } from './records.service';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +16,8 @@ import { HelloComponent } from './hello/hello.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [RecordsService],
   bootstrap: [AppComponent]
