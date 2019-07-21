21808db750eb68b029a39479b5d4fba42e48a142
amenraprod <amenraprod@gmail.com>
2019-07-21 02:56:28 -0400

    moved function outside constructor (mistake)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,9 @@ export class AppComponent {
 
 
   constructor() {
-    this.myVariable=""
-    this.mydisabledValue = false;
-    setInterval(() => {
-    this.myVariable = Math.random().toString()
-    }, 500)
-    this.mydisabledValue = Math.random() > 0.5 ;
-    myFunction(){
-      console.log("You called me again!");
-    }
+
   }
+
+  myFunction(){
+    console.log("You called me again!");
 }
