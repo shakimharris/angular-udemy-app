0e56e89129f91e0cc921a464e9e4f191645fbd5d
amenraprod <amenraprod@gmail.com>
2019-07-21 15:33:53 -0400

    changed json file to php access control

---
+++ tests/file.php
@@ -0,0 +1,36 @@
+<?php 
+header('Access-Control-Allow-Origin *')
+?>
+{
+obj : [
+    {
+      name: 'Shakim',
+      address: '176 John Daly'
+    },
+    {
+      name: 'Kristina', 
+      address: '165 Vincent'
+    },
+    {
+      name: 'Shakim',
+      address: '176 John Daly'
+    },
+      {
+      name: 'Kristina', 
+      address: '165 Vincent'
+    },
+    {
+      name: 'Shakim',
+      address: '176 John Daly'
+    },
+      {
+      name: 'Kristina', 
+      address: '165 Vincent'
+      },
+    {
+      name: 'Rihanna',
+      address: false
+    }
+  ]
+}
+  
