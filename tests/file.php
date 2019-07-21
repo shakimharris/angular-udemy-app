8ec18abf7e796b355c9534bc9843f7597164e342
amenraprod <amenraprod@gmail.com>
2019-07-21 17:55:56 -0400

    rm list

--- tests/file.php
+++ tests/file.php
@@ -3,42 +3,12 @@ header('Access-Control-Allow-Origin: *');
 //header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Content-Type: application/json');
 ?>
-{
+
 obj : [
     {
-      name: 'Shakim',
-      address: '176 John Daly'
-    },
-
-    {
-      name: 'Kristina', 
-      address: '165 Vincent'
-    },
-
-    {
-      name: 'Shakim',
-      address: '176 John Daly'
-    },
-
-    {
-      name: 'Kristina', 
-      address: '165 Vincent'
-    },
-    
-    {
-      name: 'Shakim',
-      address: '176 John Daly'
-    },
-    
-    {
-      name: 'Kristina', 
-      address: '165 Vincent'
-      },
-      
-    {
-      name: 'Rihanna',
-      address: false
+      name: "Shakim",
+      address: "176 John Daly"
     }
   ]
-}
+
   
