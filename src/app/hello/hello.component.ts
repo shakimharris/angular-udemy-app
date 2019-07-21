3c3236aa772820fc1bc48a9a3ca45272e9cb7328
amenraprod <amenraprod@gmail.com>
2019-07-21 02:10:59 -0400

    set times to 50 with math func

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -10,15 +10,19 @@ export class HelloComponent implements OnInit {
   bitwiseOR = 2 | 5 // 010 101 => 0 | 1 = 1 then 1 |0 is 1 |  then 0 | 1 = 1; 111
 
   i=0;
+  someVar = Math.random;
 
   constructor() {
   }
 
   dosomeHeavyTask(){
-    console.log(`Called ${this.i++} + times`)
+    console.log("Called ${this.i++} + times")
   }
   
   ngOnInit() {
+    setInterval(()=>{
+      this.someVar = Math.random()
+    }, 50)
   }
 
 }
