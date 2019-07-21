98f5719ed40b660502ed2718a8de547af2d78e3b
amenraprod <amenraprod@gmail.com>
2019-07-21 02:37:56 -0400

    added interval to value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,5 @@
 import { Component } from '@angular/core';
 
-function log (target, name, descriptor){
-  console.log(target, name, descriptor);
-  // store the original function in a variable
-  // manipulate the descriptor.value
-  // return the descriptor
-
-  const original = descriptor.value
-  // manipulate the descriptor.value completed
-
-  descriptor.value = function (args: any){
-    console.log("Arguments", args, "were passed in this function!");
-    const result = original.apply(this, args);
-    console.log("The results of the function are" , result);
-  };
-
-  descriptor.value = function () {
-    console.log("This function was hacked!");
-  }
-  return descriptor;
-  // descriptor returned here
-}
 
 @Component({
   selector: 'app-root',
@@ -29,13 +8,12 @@ function log (target, name, descriptor){
 })
 export class AppComponent {
   myVariable= 'Udemy App';
+this.myVariable = ""
 
-  constructor(){
-    console.log("This statement was created by the constructor method", this.aSimpleMethod(5));
-  }
-
-  @log
-  aSimpleMethod (a) {
-  return a*a;
+  constructor() {
+    setInterval(() => {
+    this.myVariable = Math.random.toString();
+    
+    }, 500)
   }
 }
