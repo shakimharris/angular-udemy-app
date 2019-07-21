96046276f6dd2a2d183a39a9181ec7ad70bf9e07
amenraprod <amenraprod@gmail.com>
2019-07-21 13:36:53 -0400

    added http client

--- src/app/records.service.ts
+++ src/app/records.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordsService {
 
-  constructor() { }
+  constructor(private http: HttpClient]) { }
 
-  getData (){
+  getData() {
     return [
       {
         name: 'Shakim',
